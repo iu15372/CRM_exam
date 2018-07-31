4cb196577b0948c88f506a7a96139036e5c1d350
AnastasiyaT <iu23042008>
2018-07-31 16:01:08 +0300

    first commit

---
+++ src/main/java/CRM_exam/App.java
@@ -0,0 +1,13 @@
+package CRM_exam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
